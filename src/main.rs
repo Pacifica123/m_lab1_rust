3212a0f594da3b745c52f2232894a81f340f2cf2
Noir <accountforprogrammingmiandedge@gmail.com>
2024-02-17 19:56:16 +0700

    чтение кода

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,21 @@ fn main(){
     let (a, b, n, hi_sqr_theory) = read_params(filename).expect("Файл не найден!");
     println!("Параметры успешно считаны из файла {}", filename);
 
-    let k = (1.0 + 3.322 * f64::log10(n as f64)).ceil() as usize; 
+    let k = (1.0 + 3.322 * f64::log10(n as f64)).ceil() as usize; // формула Стерджеса
     let R = get_random(a, b, n as i32);
-    let R_clone = R.clone();
     
 
-    let mean_X = mean(R);
+    let mean_X = mean(&R);
     let mean_theory = (a + b) / 2.0;
-    let dispersion_X = dispersion(&R_clone);
-    let dispersion_theory = (b - a) * (b - a) / 12.0;
-    let hi_sqr = hi_squared(&R_clone, a, b,  k);
+    let dispersion_X = dispersion(&R);
+    let dispersion_theory = (b - a).powi(2) / 12.0;
+    let hi_sqr = hi_squared(&R, a, b,  k);
 
     // вывод в текстовый файл
     let mut results = String::new();
     results.push_str(&format!("Входные параметры: a = {}, b = {}, N = {}\n", a, b, n));
     results.push_str(&format!("Количество интервалов: {}\n", k));
-    results.push_str(&format!("Сгенерированнная выборка: {:?}\n", R_clone));
+    results.push_str(&format!("Сгенерированнная выборка: {:?}\n", &R));
     results.push_str(&format!("Математическое ожидание (теоретическое): {}\n", mean_theory));
     results.push_str(&format!("Математическое ожидание (расчетное): {}\n", mean_X));
     results.push_str(&format!("Дисперсия (теоретическая): {}\n", dispersion_theory));
@@ -56,13 +55,13 @@ fn get_random (
 }
 
 // математическое ожидание
-fn mean(R: Vec<f64>) -> f64 {
+fn mean(R: &Vec<f64>) -> f64 {
     R.iter().sum::<f64>() / R.len() as f64
 }
 
 // дисперсия
 fn dispersion(R: &Vec<f64>) -> f64 {
-    let m = mean(R.to_vec());
+    let m = mean(R);
     let mut sum_of_squares: f64 = 0.0;
     for x in R {
         let diff = *x as f64 - m;
@@ -78,7 +77,8 @@ fn hi_squared(R: &Vec<f64>, a: f64, b: f64,  k: usize) -> f64 {
     for i in 0..k {
         let mut n_counts = 0;
         for x in R {
-            if *x >= a + ((i)  as f64) * (b - a) / (k as f64) && *x < a + ((i + 1) as f64) * (b - a) / (k as f64) {
+            if *x >= a + ((i)  as f64) * (b - a) / (k as f64) 
+            && *x < a + ((i + 1) as f64) * (b - a) / (k as f64) {
                 n_counts += 1;
             }
         }
@@ -92,7 +92,7 @@ fn hi_squared(R: &Vec<f64>, a: f64, b: f64,  k: usize) -> f64 {
 
 }
 
-// СЛУЖЕБНЫЕ ФУНКЦИИ
+// ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
 fn read_params(filename: &str) -> Result<(f64, f64, usize, Vec<f64>), io::Error>{
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
@@ -100,13 +100,10 @@ fn read_params(filename: &str) -> Result<(f64, f64, usize, Vec<f64>), io::Error>
     let params = lines.next().expect("Первая строка отсутствует!")?;
     let hi_sqr_theory_line = lines.next().expect("Вторая строка отсутствует!")?;
 
-    
-    println!("Содержимое файла:");
-    println!("{}", params);
-    println!("{}", hi_sqr_theory_line);
-
-
-
+    // debug:
+    // println!("Содержимое файла:");
+    // println!("{}", params);
+    // println!("{}", hi_sqr_theory_line);
     
     let a: f64 = params.split_whitespace().next().unwrap().parse().expect("Некорректное значение для a!");
     let b: f64 = params.split_whitespace().nth(1).unwrap().parse().expect("Некорректное значение для b!");
